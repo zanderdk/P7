0f52a18c1049646d463d466ad2f02c7fe96e7541
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-09-30 09:03:43 +0200

    better link populator

--- link-graph-db-populator/parser.py
+++ link-graph-db-populator/parser.py
@@ -1,16 +1,24 @@
 import re
 import sys
+import fileinput
+
 
 from neo4j.v1 import GraphDatabase, basic_auth
 
-driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "1234"))
-
+driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "12345"))
 
 
 regex = re.compile("<(.*?)> <.*?> <(.*?)> \.")
+counter = 0
+session = driver.session()
+
 
 def parse(line):
-    session = driver.session()
+    global session
+    if counter % 10000 == 0:
+      session.close()
+      session = driver.session()
+      print("flushed at " + str(counter))
     if line[0] == '#':
         # do not process comments
         return
@@ -18,14 +26,14 @@ def parse(line):
     if re_res is None:
         sys.exit("error parsing line: " + line)
     else:
-        from_link = re_res.group(1)
-        to_link = re_res.group(2)
-        session.run("CREATE (a:Link {name:{from_name}}) -[:LinksTo]-> (b:Link {name:{to_name}})", {"from_name": from_link, 
+      from_link = re_res.group(1)
+      to_link = re_res.group(2)
+      session.run("CREATE (a:Link {name:{from_name}}) -[:LinksTo]-> (b:Link {name:{to_name}})", {"from_name": from_link, 
                                                            "to_name": to_link})
-        session.close()
 
-
-with open("/home/simon/page_links_en.ttl") as fileobject:
-  for line in fileobject:
-    parse(line)
+for idx, line in enumerate(fileinput.input()):
+  counter = idx
+  parse(line)
     
+# flush the final statements
+session.close()
