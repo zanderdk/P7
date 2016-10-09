4052c392cd057e57aa6eac539bccc44a663f84c6
zander <akrog13@student.aau.dk>
2016-10-09 19:31:48 +0200

    viewCounter update

--- scripts/pageCounter/viewCounter.py
+++ scripts/pageCounter/viewCounter.py
@@ -11,9 +11,10 @@ def setViewCount(title, viewCount):
     global session
     global counter
     query = """
-            MATCH (a:Page) 
-            WHERE a.title = {title}
-            SET a.viewCount = {count}
+            MATCH (a:Page {title: {title} }) 
+            WITH sum(a.viewCount) as cur 
+            MATCH (a:Page {title: {title} }) 
+            SET a.viewCount = cur + {count}
             RETURN a.viewCount
             """
     session.run(query, {'title':title, 'count':viewCount })
@@ -24,19 +25,13 @@ def setViewCount(title, viewCount):
     counter += 1
 
 
-dictionary = {}
-
 for line in sys.stdin:
     try:
         cols = line.split()
         if 'en' == cols[0] and len(cols) == 4:
-            if cols[1] in dictionary:
-                dictionary[cols[1]] += int(cols[2])
-            else:
-                dictionary[cols[1]] = int(cols[2])
+            title = cols[1]
+            amount = int(cols[2])
+            setViewCount(title, amount)
     except Exception:
         continue
 
-
-for x,y in dictionary.items():
-   setViewCount(x,y)
