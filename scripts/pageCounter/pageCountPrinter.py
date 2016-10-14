96aa04c0be9e8eae60751b2c3402a04804004927
Kasper Kohsel Terndrup <STUDENT\kternd13@ifs-nix-1404-template>
2016-10-14 09:15:37 +0200

    Page Count Printer coded on server through phone

---
+++ scripts/pageCounter/pageCountPrinter.py
@@ -0,0 +1,35 @@
+import sys
+
+dictionary = {}
+
+for line in sys.stdin:
+    try:
+        cols = line.split()
+        if 'en' == cols[0] and len(cols) == 4:
+            if cols[1] in dictionary:
+                dictionary[cols[1]] += int(cols[2])
+            else:
+                dictionary[cols[1]] = int(cols[2])
+    except Exception:
+        continue
+
+for key,value in dictionary.items():
+    print(key, value)
+
+#i = 1
+#with open("2016_02_en_clickstream.tsv", "rt") as clickStream:
+#    for x in clickStream:
+#        if i > 1:
+#            cols = x.split()
+#            source = cols[0]
+#            linkType = cols[2]
+#            if source not in dictionary or "link" not in linkType:
+#                continue
+#            target = cols[1]
+#            amount = cols[3]
+#            Ns = dictionary[source]
+#            pst = float(amount)/Ns
+#            sys.stdout.write(source + "\t" + target + "\t" + str(amount) + "\t" + str(pst)
+#            print()
+#        i += 1
+
