1475b66b56678e0ae40427613bdbd4fb787118fe
Lasse Just <lasse_just@outlook.com>
2016-10-15 23:18:21 +0200

    Refactoring, print header, use csv reader/writer, better status print

--- scripts/featureExtraction/findTrainingPairs.py
+++ scripts/featureExtraction/findTrainingPairs.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
-from neo4j.v1 import GraphDatabase, basic_auth
-import io
 import sys
+import csv
+from neo4j.v1 import GraphDatabase, basic_auth
 
+outputColumns = ["source", "target", "click_rate"]
 
-if len(sys.argv) > 1:
+def findTrainingPairs(outputFile):
     driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "12345"))
     session = driver.session()
-    with io.open(sys.argv[1], "w+", encoding="utf-8") as f:
+
+    with open(outputFile, "w+", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=" ")
+        writer.writerow(outputColumns)   # Write header
         query = '''
-            MATCH (a:Page)-[r:clickStream]->(b:Page) 
+            MATCH (a:Page)-[r:clickStream]->(b:Page)
             WHERE exists(a.featured) AND a <> b
             RETURN a.title, b.title, r.clickRate'''
         res = session.run(query)
         for record in res:
-            line = " ".join([str(val) for val in record.values()]) + "\n"
-            f.write(line)
-    session.close()
-    print("Done")
+            writer.writerow(record.values())
 
+    # Done, close session
+    session.close()
+
+if len(sys.argv) > 1:
+    findTrainingPairs(sys.argv[1])
+    print("Done")
 else:
     print("Must specify output file")
--- scripts/featureExtraction/generateTrainingData.py
+++ scripts/featureExtraction/generateTrainingData.py
@@ -1,46 +1,68 @@
 # Call with path to input file and output file as parameters
-import io
 import sys
+import csv
 from neo4j.v1 import exceptions
 import PairedFeatureExtractor as ext
+
+# Columns names, in order, used to print the header
+outputColumns = ["path", "outgoing", "incoming", "keywords", "pageviews", "click_rate"]
 
 # Input: File with training pairs on the form:
 #       fromTitle toTitle label
 # Output: File with training data on the form:
-#       ShortestPath commonOutgoing commonIncoming keywordSimilarity pageViewsRatio label
-def generateTrainingData(inputFile, outputFile):
-    with io.open(inputFile, "r", encoding="utf-8") as f:
-        with io.open(outputFile, "w+", encoding="utf-8") as outputFile:
+#       shortestPath commonOutgoing commonIncoming keywordSimilarity pageViewsRatio label
+
+def generateTrainingData(inputFilePath, outputFilePath):
+    with open(inputFilePath, "r", newline="", encoding="utf-8") as inputFile:
+        reader = csv.reader(inputFile, delimiter=" ")
+
+        # Check for header, rewind input file, skip header if present
+        hasHeader = csv.Sniffer().has_header(inputFile.read(1024))
+        inputFile.seek(0)
+        if hasHeader:
+            print("Skipped header")
+            next(reader)
+
+        with open(outputFilePath, "w+", newline="", encoding="utf-8") as outputFile:
+            writer = csv.writer(outputFile, delimiter=" ")
+            writer.writerow(outputColumns)   # Write header
             extractor = ext.PairedFeatureExtractor()
+
+            # Counters for keeping status
             counter = 1
             noPathCounter = 0
-            failedCounter = 0
-            for line in f:
-                pairEntry = line.split()
+            noneCounter = 0
+            exceptionCounter = 0
+
+            # Iterate over all input lines, using the csv reader
+            for row in reader:
                 try:
-                    features = extractor.extractFeatures(pairEntry[0], pairEntry[1])
-                    dataPoint = features + (pairEntry[2],)
-                    if all(field is not None for field in dataPoint):
-                        if dataPoint[0] is 0:
+                    features = extractor.extractFeatures(row[0], row[1])
+                    if all(field is not None for field in features):
+                        dataPoint = features + (row[2],)
+                        if dataPoint[0] == 0.0:
                             noPathCounter += 1
-                        line = " ".join([str(x) for x in dataPoint]) + "\n"
-                        outputFile.write(line)
+                        writer.writerow(dataPoint)
                     else:
-                        failedCounter += 1
-                    if counter % 1000 is 0:
-                        printStatus(counter, failedCounter, noPathCounter)
+                        noneCounter += 1
+                    if counter % 1000 == 0:
+                        printStatus(counter, noneCounter, exceptionCounter, noPathCounter)
                     counter += 1
                 except exceptions.CypherError:
-                    failedCounter += 1
+                    exceptionCounter += 1
                     counter += 1
                     continue
-            print("Done")
-            printStatus(counter - 1, failedCounter, noPathCounter)
 
-def printStatus(pairCounter, failedCounter, noPathCounter):
-    print("Processed pairs: " + str(pairCounter))
-    print("Failed pairs: " + str(failedCounter))
-    print("Pairs without path: " + str(noPathCounter) + "\n")
+            # Done, print final status
+            print("\nDone! Final status:")
+            printStatus(counter - 1, noneCounter, exceptionCounter, noPathCounter)
+
+def printStatus(pairCount, noneCount, exceptionCount, noPathCount):
+    failedCount = noneCount + exceptionCount
+    print("\nProcessed pairs: " + str(pairCount))
+    print("Failed pairs: %d (%d exceptions, %d None)" %(failedCount, exceptionCount, noneCount))
+    print("Generated data points: " + str(pairCount - failedCount))
+    print("Pairs without path: " + str(noPathCount))
 
 if len(sys.argv) > 2:
     generateTrainingData(sys.argv[1], sys.argv[2])
