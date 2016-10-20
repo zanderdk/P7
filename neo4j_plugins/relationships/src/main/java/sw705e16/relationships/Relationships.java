a08424e7a319a65fae7d9241b6e517cf6d7e34dd
zander <akrog13@student.aau.dk>
2016-10-20 11:51:30 +0200

    relationships plugin fixed

--- neo4j_plugins/relationships/src/main/java/sw705e16/relationships/Relationships.java
+++ neo4j_plugins/relationships/src/main/java/sw705e16/relationships/Relationships.java
@@ -1,16 +1,11 @@
 package sw705e16.relationships;
 
-import com.google.common.collect.Lists;
 import org.neo4j.graphdb.*;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
-import org.neo4j.unsafe.impl.batchimport.cache.idmapping.string.Radix;
 
-import javax.management.relation.Relation;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -21,26 +16,6 @@ public class Relationships
     @Context
     public GraphDatabaseService db;
 
-    public static class resultclass {
-        public static String title;
-        public static Double clickRate;
-
-        resultclass(String Title, Double ClickRate){
-            title = Title;
-            clickRate = ClickRate;
-        }
-
-    }
-
-    public static class RelationshipsList {
-        public static List<Object> successorsList;
-        public static List<Object> predecessorsList;
-
-        public RelationshipsList(List<Object> successors, List<Object> predecessors) {
-            successorsList = successors;
-            predecessorsList = predecessors;
-        }
-    }
 
     /**
      * Retrieves titles of predecessors and successors for an article
@@ -48,7 +23,7 @@ public class Relationships
      * @return Titles of predecessors and titles of successors.
      */
     @Procedure("getRelationships")
-    public Stream<Relationship> getRelationships(@Name("title") String title) {
+    public Stream<RelationshipWrapper> getRelationships(@Name("title") String title) {
         // Lookup title in db to find the corresponding node.
         Label pageLabel = Label.label("Page");
         Node thisNode = db.findNode(pageLabel, "title", title);
@@ -56,24 +31,24 @@ public class Relationships
         // Get all successor relationships
         Iterable<Relationship> relationIterator = thisNode.getRelationships(clickStreamType, Direction.BOTH);
 
-        List<Object> successorList = new ArrayList<>();
-        List<Object> predecessorList = new ArrayList<>();
+        Stream<Relationship> rels = StreamSupport.stream(relationIterator.spliterator(), false);
 
-        for (Relationship r : relationIterator) {
-            Double clickRate = (Double) r.getProperty("clickRate");
-            if(r.getStartNode().getId() == thisNode.getId()){
-                successorList.add((Object)new resultclass((String)r.getEndNode().getProperty("title"), clickRate));
-            } else {
-                predecessorList.add((Object)new resultclass((String)r.getStartNode().getProperty("title"), clickRate));
-            }
+        return rels.map( r -> new RelationshipWrapper(r, thisNode) );
+    }
+
+    public class RelationshipWrapper {
+
+        public Double clickRate = 0.0;
+        public Long clicks = (long)0;
+        public String direction = "Outgoing";
+        public String otherNode = null;
+
+        public RelationshipWrapper(Relationship r, Node startNode) {
+            clickRate = (Double)r.getProperty("clickRate");
+            clicks = (Long)r.getProperty("clicks");
+            direction = (r.getStartNode().getId() == startNode.getId())? "Outgoing" : "Ingoing";
+            otherNode = (String)r.getOtherNode(startNode).getProperty("title");
+
         }
-
-        List<Relationship> res = new ArrayList<>();
-
-        for (Relationship r : relationIterator){
-            res.add(r);
-        }
-
-        return StreamSupport.stream(relationIterator.spliterator(), false);
     }
 }
