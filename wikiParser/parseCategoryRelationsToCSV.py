9be00bc156adc12e14fc76816d7089572f53c3b6
Mathias Rasmussen <mathiasvr@gmail.com>
2016-10-21 22:23:16 +0200

    category relations csv

--- wikiParser/parseCategoryRelationsToCSV.py
+++ wikiParser/parseCategoryRelationsToCSV.py
@@ -1,15 +1,22 @@
-# Usage: bzcat skos_categories_en.ttl.bz2 | python3 parseCategoryRelationsToCSV.py > category_relations.csv
+# Usage: bzcat skos_categories_en.ttl.bz2 | python3 parseCategoryRelationsToCSV.py
 
 import sys
 import re
+import io
 
 turtle = re.compile(r'<http:\/\/dbpedia.org\/resource\/Category:(.+?)>\s<.*?#(.+?)>\s(.+)')
 
 hashstuff = re.compile(r'.+#(.+?)>')
 category = re.compile(r'<http:\/\/dbpedia.org\/resource\/Category:(.+?)>')
 
-# header
-print('category1,category2,relationName')
+broaderRels = open('category_relations_broader.csv', 'w', encoding='utf-8')
+relatedRels = open('category_relations_related.csv', 'w', encoding='utf-8')
+
+# headers
+broaderRels.write('category1,category2\n')
+relatedRels.write('category1,category2\n')
+
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 
 for line in sys.stdin:
   # skip comments
@@ -30,7 +37,9 @@ for line in sys.stdin:
     # create BROADER or RELATED relation
     cat2 = category.match(rest).group(1)
     #createRelation({ 'category1': cat, 'category2': cat2, 'relationName': typ.upper() })
-    print('"' + cat + '","' + cat2 + '",' + typ.upper())
+    #print('"' + cat + '","' + cat2 + '",' + typ.upper())
+    (broaderRels if typ == 'broader' else relatedRels).write('"' + cat + '","' + cat2 + '"\n')
+    
   # TODO: create csv for attributes as well?
   #elif typ == 'type':
     # set type attribute
@@ -40,3 +49,6 @@ for line in sys.stdin:
     # set label attribute
     #val = rest.split('"')[1]
     #setAttribute({ 'category': cat, 'attribute': 'label', 'value': val })
+
+broaderRels.close()
+relatedRels.close()
