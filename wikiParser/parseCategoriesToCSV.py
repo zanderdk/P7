de61e04599fd914c6925ee38ad2db84c53b7c70b
Mathias Rasmussen <mathiasvr@gmail.com>
2016-10-21 10:53:44 +0200

    csv stuff

---
+++ wikiParser/parseCategoriesToCSV.py
@@ -0,0 +1,21 @@
+# Usage: bzcat article_categories_en.ttl.bz2 | python3 parseCategoriesToCSV.py > out.csv
+import sys
+import re
+from neo4j.v1 import GraphDatabase, basic_auth
+
+turtle = re.compile(r'<http:\/\/dbpedia.org\/resource\/(.+?)>\s<.*?>\s<http:\/\/dbpedia.org\/resource\/Category:(.+?)>')
+
+print('article_title,category_name')
+
+for line in sys.stdin:
+  if line.startswith('#'):
+    continue
+
+  m = turtle.match(line)
+
+  if (m == None):
+    print("Error parsing line:", line, file=sys.stderr)
+    continue
+
+  #createRelation({ 'article': m.group(1), 'category': m.group(2) })
+  print(m.group(1) + ',' + m.group(2))
