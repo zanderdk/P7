b726d698271afcd998c09b7c739bf801a4427c77
Michael <micjen12@student.aau.dk>
2016-10-21 11:35:48 +0200

    det virker ikke

--- scripts/featureExtraction/PairedFeatureExtractor.py
+++ scripts/featureExtraction/PairedFeatureExtractor.py
@@ -8,19 +8,29 @@ class PairedFeatureExtractor:
         self.pathLimit = pathLimit
         self.wantedFeatures = wantedFeatures
         self.feature_function_dict = { # if None, then it is special cased
-            "pathWeight": self._shortestPath,
-            "keywordsA": None,
-            "keywordsB": None,
-            "categoriesA": None,
-            "categoriesB": None,
-            "word2vec":  None, #self.word2vec.compareKeywordSets,
-            "PredesccorJaccard": self._getPredecessorJaccard,
-            "SucessorJaccard": self._getSucessorJaccard,
+            "pathWeight": lambda dict, articleA, articleB : self._shortestPath(dict, articleA, articleB),
+            "keywords": lambda dict, articleA, articleB : self._getKeywords(dict, articleA, articlesB),
+            "categories": lambda dict, articleA, articleB : self._getCategories(dict, articleA, articleB),
+            "word2vec":  lambda dict, articleA, articleB : self.word2vec.extractWord2vec(dict, articleA, articleB),
+            "predecessorJaccard": lambda dict, articleA, articleB : self._getPredecessorJaccard(dict, articleA, articleB),
+            "successorJaccard": lambda dict, articleA, articleB : self._getSuccessorJaccard(dict, articleA, articleB)
         }
 
         self._prevFrom = {"name": "", "outgoing": [], "incoming": []}
         self._prevTo = {"name": "", "outgoing": [], "incoming": []}
 
+    def featureTofieldNames(feature):
+        dict = {
+            "pathWeight": ["pathWeight"],
+            "keywords": ["keywordsA", "keywordsB"],
+            "categories": ["categoriesA", "categoriesB"],
+            "word2vecSimilarity": ["word2vecSimilarity"],
+            "word2vecBuckets": ["word2vec_b0", "word2vec_b1", "word2vec_b2", "word2vec_b3", "word2vec_b4", "word2vec_b5", "word2vec_b6", "word2vec_b7", "word2vec_b8", "word2vec_b9"],
+            "predecessorJaccard": ["predecessorJaccard"],
+            "successorJaccard": ["successorJaccard"]
+        }
+        return dict[feature]
+        
     def _runQuery(self, query, mapping):
         result = []
         with self.driver.session() as session:
@@ -34,7 +44,7 @@ class PairedFeatureExtractor:
         return [None] if not result else result
 
 
-    def _getKeywords(self, article):
+    def _getKeywords(self, dict, articleA, articleB):
         query = '''
             MATCH (a:Page)
             WHERE a.title = {article}
@@ -44,7 +54,8 @@ class PairedFeatureExtractor:
         nameMapping = {
             "article": article
         }
-        return self._runQuery(query, nameMapping)[0]
+        keywordsA = self._runQuery(query, nameMapping)
+        dict["keywordsA"]
 
     def _getCategories(self, article):
         query = '''
@@ -106,23 +117,23 @@ class PairedFeatureExtractor:
 
         return inter/union if union != 0 else 0
 
-    def _getSucessorJaccard(self, fromLink, toLink):
-        return self._getJaccard(fromLink, toLink, "outgoing")
+    def _getSuccessorJaccard(self, dict, fromLink, toLink):
+        value = self._getJaccard(fromLink, toLink, "outgoing")
+        dict["successorJaccard"] = value
 
-    def _getPredecessorJaccard(self, fromLink, toLink):
-        return self._getJaccard(fromLink, toLink, "incoming")
+    def _getPredecessorJaccard(self, dict, fromLink, toLink):
+        value = self._getJaccard(fromLink, toLink, "incoming")
+        dict["predecessorJaccard"] = value
 
-
-
-
-    def _shortestPath(self, fromLink, toLink):
+    def _shortestPath(self, dict, fromLink, toLink):
         query = "CALL weightedShortestPathCost({fromLink}, {toLink}, {pathLimit})"
         nameMapping = {
             "fromLink": fromLink,
             "toLink": toLink,
             "pathLimit": self.pathLimit
         }
-        return self._runQuery(query, nameMapping)[0]
+        value = self._runQuery(query, nameMapping)[0]
+        dict["pathWeight"] = value
 
     def get_field_names(self):
         """Returns a list of feature names. Only the wanted features are returned."""
@@ -134,7 +145,7 @@ class PairedFeatureExtractor:
         """Returns a list of all feature names."""
         return self.feature_function_dict.keys()
 
-    def extractFeatures(self, fromArticle, toArticle):
+    def extractFeatures(self, articleA, articleB):
         """Returns all extracted features as a dictionary
         """
         res_dict = {}
@@ -142,20 +153,6 @@ class PairedFeatureExtractor:
         for wantedFeature in self.wantedFeatures:
             # feature_function_dict finds the function that returns the feature. Call it.
             feature_func = self.feature_function_dict[wantedFeature]
-
-            # If it is None, do not do anything, as it is special cased
-            if feature_func is not None:
-                res_dict[wantedFeature] = feature_func(fromArticle, toArticle)
-        
-        # special cases for single parameter functions
-        if "keywordsA" in self.wantedFeatures:
-            res_dict["keywordsA"] = self._getKeywords(fromArticle)
-        if "keywordsB" in self.wantedFeatures:
-            res_dict["keywordsB"] = self._getKeywords(toArticle)
-        
-        if "categoriesA" in self.wantedFeatures:
-            res_dict["categoriesA"] = self._getCategories(fromArticle)
-        if "categoriesB" in self.wantedFeatures:
-            res_dict["categoriesB"] = self._getCategories(toArticle)
-        
+            feature_func(res_dict, articleA, articleB)
+            
         return res_dict
