2fbb841b84200192417eea27451a3ba0c2d80f13
Abstrakten <kterndrupxd@gmail.com>
2016-11-02 14:30:54 +0100

    website stuff

--- web/wiki/catagories/urls.py
+++ web/wiki/catagories/urls.py
@@ -6,6 +6,8 @@ app_name = 'catagories'
 urlpatterns = [
     # ex: /catagories/
     url(r'^$', views.index, name='index'),
+    # ex: /search
+    url(r'^search/$', views.search, name='search'),
     # ex: /catagories/biology
     url(r'^(?P<catagory_name>[a-z]+)/$', views.catagory, name='catagory'),
 ]
