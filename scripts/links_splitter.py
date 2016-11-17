b7d04c33a46d30d40a42edee7b5e7b06c6ef597b
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-11-17 18:20:44 +0100

    push

--- scripts/links_splitter.py
+++ scripts/links_splitter.py
@@ -10,16 +10,16 @@ with open("featured_articles.csv", "r") as in_featured_articles:
 
 from_is_featured = []
 
-with open("new_links.csv", "w") as out_new_links:
+#with open("new_links.csv", "w") as out_new_links:
    # write header
-   out_new_links.write(":START_ID :END_ID\n")
+#   out_new_links.write(":START_ID :END_ID\n")
    # load links.csv
-   for line in sys.stdin:
-       from_title, to_title = line.split()
-       if from_title in featured_articles:
-           from_is_featured.append((from_title, to_title))
-       else:
-           out_new_links.write("{} {}\n".format(from_title, to_title))
+for line in sys.stdin:
+    from_title, to_title = line.split()
+    if from_title in featured_articles:
+        from_is_featured.append((from_title, to_title))
+#       else:
+#           out_new_links.write("{} {}\n".format(from_title, to_title))
 
 # shuffle from_is_featured
 random.shuffle(from_is_featured)
@@ -32,12 +32,14 @@ rest_count = int(math.floor(total_count*0.40))
 
 # extract test data (20%)
 test_data = from_is_featured[:test_count]
-
+print(len(test_data))
 # extract training data (40%)
 training_data = from_is_featured[test_count:test_count+training_count]
+print(len(training_data))
 
 # extract rest data (40%)
 walk_data = from_is_featured[test_count+training_count:]
+print(len(walk_data))
 
 def write_to_file(file_name, data, header):
     with open(file_name, "w") as out_data:
