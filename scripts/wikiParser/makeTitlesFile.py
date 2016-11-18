23c5ff7b1c92ea806a9687b85655bcf7b4017d6c
Mathias Rasmussen <mathiasvr@gmail.com>
2016-11-18 08:50:36 +0100

    saoeu

--- scripts/wikiParser/makeTitlesFile.py
+++ scripts/wikiParser/makeTitlesFile.py
@@ -1,14 +1,21 @@
 import sys
 
-titles = set() 
+redirects = set()
+
+with open("redirect_pages.csv", "r", encoding="utf-8") as redirects_pages_file:
+  for line in redirects_pages_file:
+    redirects.add(line.split()[0])
+
+titles = set()
 
 for line in sys.stdin:
   fromtitle, totitle = line.split()
 
   titles.add(fromtitle)
-  titles.add(totitle)
+
+  if totitle not in redirects:
+    titles.add(totitle)
 
 print('title:ID')
 for key in titles:
   print(key)
-  
