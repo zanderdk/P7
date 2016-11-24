4aed8c51bdcc2cdcaec81279ed80c9d3e34e5ff9
Mathias Rasmussen <mathiasvr@gmail.com>
2016-11-24 15:22:30 +0100

    wtf

--- random-negatives.py
+++ random-negatives.py
@@ -4,7 +4,6 @@ from neo4j.v1 import GraphDatabase, basic_auth
 driver = GraphDatabase.driver("bolt://localhost:10001", auth=basic_auth("neo4j", "12345"))
 
 session = driver.session()
-#counter = 1
 
 def getNotLinkedTitle(from_title):
   #global counter
@@ -14,16 +13,22 @@ def getNotLinkedTitle(from_title):
   #  print("flushed at " + str(counter))
 
   title = None 
-  while title == None:
+  counter = 0
+  while title == None and counter < 10:
     #res = session.run("match (a:FeaturedPage {title: '" + from_title + "'}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|TRAINING_DATA|TEST_DATA|REDIRECTS_TO]->(b) return b.title")
-    res = session.run("match (a:FeaturedPage {title: '" + from_title +"'}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|:TRAINING_DATA|TEST_DATA]->(b) AND NOT (a)-[:LINKS_TO]->()-[:REDIRECTS_TO]->(b) return b.title")
+    counter += 1
+    res = session.run("match (a:FeaturedPage {title: {title}}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|:TRAINING_DATA|TEST_DATA]->(b) AND NOT (a)-[:LINKS_TO]->()-[:REDIRECTS_TO]->(b) return b.title", {'title': from_title})
     for r in res:
       title = r[0]
-  
+    
   return title
 
 for line in sys.stdin:
   featured_title = line.split()[0]
   #print(featured_title)
   to_title = getNotLinkedTitle(featured_title)
-  print(featured_title + ' ' + to_title)
+
+  if to_title == None:
+    print('#Error, failed getting negative for', featured_title)
+  else:
+    print(featured_title + ' ' + to_title)
