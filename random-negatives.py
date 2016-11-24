3ff72111c40be6d94100e50f738b28831dd006dd
Mathias Rasmussen <mathiasvr@gmail.com>
2016-11-24 14:44:55 +0100

    :tada:

--- random-negatives.py
+++ random-negatives.py
@@ -4,8 +4,15 @@ from neo4j.v1 import GraphDatabase, basic_auth
 driver = GraphDatabase.driver("bolt://sw705e16.cs.aau.dk:10001", auth=basic_auth("neo4j", "12345"))
 
 session = driver.session()
+counter = 1
 
 def getNotLinkedTitle(from_title):
+  global counter
+  if counter % 10000 == 0:
+    session.close()
+    session = driver.session()
+    print("flushed at " + str(counter))
+
   title = None 
   while title == None:
     #res = session.run("match (a:FeaturedPage {title: '" + from_title + "'}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|TRAINING_DATA|TEST_DATA|REDIRECTS_TO]->(b) return b.title")
