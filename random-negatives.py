2a1cd453e263809532c057d9bd61fb7d9fa50c80
Mathias Rasmussen <mathiasvr@gmail.com>
2016-11-24 15:25:27 +0100

    igen

--- random-negatives.py
+++ random-negatives.py
@@ -17,7 +17,7 @@ def getNotLinkedTitle(from_title):
   while title == None and counter < 10:
     #res = session.run("match (a:FeaturedPage {title: '" + from_title + "'}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|TRAINING_DATA|TEST_DATA|REDIRECTS_TO]->(b) return b.title")
     counter += 1
-    res = session.run("match (a:FeaturedPage {title: {title}}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|:TRAINING_DATA|TEST_DATA]->(b) AND NOT (a)-[:LINKS_TO]->()-[:REDIRECTS_TO]->(b) return b.title", {'title': from_title})
+    res = session.run("match (a:FeaturedPage {title: {title}}),(b:Page) where id(b) = toInteger(Rand()*11159211) AND NOT (a)-[:LINKS_TO|TRAINING_DATA|TEST_DATA]->(b) AND NOT (a)-[:LINKS_TO]->()-[:REDIRECTS_TO]->(b) return b.title", {'title': from_title})
     for r in res:
       title = r[0]
     
