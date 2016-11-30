bc4964b335699025711acc01e67e1893ea9d47cb
Abstrakten <kterndrupxd@gmail.com>
2016-11-30 14:19:47 +0100

    api stuff

--- web/wiki/API/urls.py
+++ web/wiki/API/urls.py
@@ -1,9 +1,30 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
 from . import views
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 app_name = 'API'
 urlpatterns = [
-    # ex: /missing/
-    url(r'^missing/$', views.full_pool, name='fullPool'),
+    # ex: /links/5/
+    url(r'^links/(?P<count>[0-9]+)/$', views.partial_pool, name='partialPool'),
+    # ex: /links/
+    url(r'^links/$', views.full_pool, name='fullPool'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- web/wiki/API/views.py
+++ web/wiki/API/views.py
@@ -3,16 +3,26 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 import os
 from django.conf import settings
-import json
+import simplejson as json
+import math
 
 # Create your views here.
 
-def full_pool(request):
-
-    output = ""
+def _get_pool_pairs(MaxCount = math.inf):
+    counter = 0
+    output = []
     with open(os.path.join(settings.PROJECT_ROOT, 'pool')) as csvPool:
         for row in csvPool:
-            output += row   
+            if MaxCount <= counter:
+                break
+            source, target = row.split()
+            output.append({"source": source, "target": target})
+            counter += 1
+    return json.dumps(output, sort_keys = True)
 
-    latest_question_list = range(1,10)
-    return HttpResponse(output)
+
+def full_pool(request):
+    return HttpResponse(_get_pool_pairs())
+
+def partial_pool(request, count):
+    return HttpResponse(_get_pool_pairs(int(count)))
