01aa54b9e72246fcac02d680ef79963236f05ffe
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-12-05 15:18:46 +0100

    asdf

--- scripts/process-cv-results.py
+++ scripts/process-cv-results.py
@@ -6,10 +6,15 @@ def keras_baseline_model():
     pass
 
 results = []
-with open("cv_results.p", "rb") as results_file:
+with open("cv_results_2_fold.p", "rb") as results_file:
     results = pickle.load(results_file)
 
 pp = pprint.PrettyPrinter()
 print(len(results))
+
+names = list(map(lambda x: x["name"], results))
+
 for res in results:
     pp.pprint(res)
+
+print(names)
