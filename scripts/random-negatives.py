335f7935b46460d296064ac3d9ff52d9694c0821
Mathias Rasmussen <mathiasvr@gmail.com>
2016-12-06 09:51:06 +0100

    negative test

--- scripts/random-negatives.py
+++ scripts/random-negatives.py
@@ -1,9 +1,16 @@
+# python3 random-negatives.py < pos_test.tsv 
 import sys
 from neo4j.v1 import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver("bolt://localhost:10001", auth=basic_auth("neo4j", "12345"))
 
 session = driver.session()
+
+negative_training_targets = set()
+
+with open("neg_training.tsv", "r", encoding="utf-8") as negative_training_pairs:
+  for line in negative_training_pairs:
+    negative_training_targets.add(line.split()[1])
 
 def getNotLinkedTitle(from_title):
   title = None 
@@ -20,7 +27,12 @@ def getNotLinkedTitle(from_title):
 for line in sys.stdin:
   featured_title = line.split()[0]
   #print(featured_title)
+
   to_title = getNotLinkedTitle(featured_title)
+
+  # not in training data
+  while to_title in negative_training_targets:
+    to_title = getNotLinkedTitle(featured_title)
 
   if to_title == None:
     print('#Error, failed getting negative for', featured_title)
