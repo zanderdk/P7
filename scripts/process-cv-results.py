483e18c1c069932e95dddd9706229e0afb86c595
Lasse Just <lasse_just@outlook.com>
2016-12-20 00:47:07 +0100

    process 10-fold result

--- scripts/process-cv-results.py
+++ scripts/process-cv-results.py
@@ -1,20 +1,43 @@
+import sys
 import pickle
 import pprint
+import csv
 
 # Method must be available to unpickle KerasClassifier
 def keras_baseline_model():
     pass
 
+names = [
+    'Baseline',
+    'Logistic Regression',
+    'Ridge Regression',
+    'Perceptron',
+    'Passive Aggressive',
+    'Linear Discriminant Analysis',
+    'Quadratic Discriminant Analysis',
+    'Gaussian Naive Bayes',
+    'Linear Support Vector Machine',
+    'Decision Tree',
+    'Nearest Centroid',
+    'Multi Layer Perceptron',
+    'Neural Network (Keras)'
+]
+
 results = []
-with open("cv_results_2_fold.p", "rb") as results_file:
+with open("10f_results.p", "rb") as results_file:
     results = pickle.load(results_file)
 
-pp = pprint.PrettyPrinter()
-print(len(results))
+results = [(name, res["scores"]["precision"], res["scores"]["recall"]) for name, res in zip(names, results)]
+results.sort(key=lambda x: x[1], reverse=True)
+header = ("name", "precision", "recall")
 
-names = list(map(lambda x: x["name"], results))
+if len(sys.argv) > 1:
+    with open(sys.argv[1], "w+", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=";")
+        writer.writerow(header)
+        for res in results:
+            writer.writerow(res)
+    print(results)
 
-for res in results:
-    pp.pprint(res)
-
-print(names)
+else:
+    print("Specify output file")
